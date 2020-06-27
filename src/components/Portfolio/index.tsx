d1a93a5acecd01c260a0b1f80ef850d044e9eb58
Doowankim <rlaendhks11@gmail.com>
2020-06-27 23:40:57 +0900

    feat: Profile & Portfolio page added

---
+++ src/components/Portfolio/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Portfolio = () => {
+  return (
+    <div>
+      <div>Portfolio Page</div>
+    </div>
+  )
+}
+
+export default Portfolio
--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
-import Home from '../Home';
-import Main from '../Main';
+import Home from "../Home"
+import Main from "../Main"
+import Profile from "../Profile"
+import Portfolio from "../Portfolio"
 
 const Router = () => {
   return (
@@ -11,10 +13,12 @@ const Router = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/main" component={Main} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/portfolio" component={Portfolio} />
         </Switch>
       </>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default Router;
+export default Router
