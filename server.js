afb78512a5a9e2caae8d48682b9210dd90cbc4b3
doowankim <rlaendhks11@gmail.com>
2021-01-02 22:35:18 +0900

    mongoose setting

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const koa = require('koa');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = new koa();
-const port = 3200;
+const port = process.env.PORT;
+
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('MongoDB Connected...'))
+	.catch(err => console.log(err));
 
 app.listen(port, console.log(`Server running on port: ${port}`));
