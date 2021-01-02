40baa5c484bf1def3fe0f3de4aa3d384ef437b21
doowankim <rlaendhks11@gmail.com>
2021-01-03 00:32:44 +0900

    Added posts model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,13 @@
 const Router = require('koa-router');
-
 const router = new Router();
 
+// @route GET localhost:3200/posts
+// @desc posts route
+// @access Private
 router.get('/posts', (req, res) => {
-	req.body = 'success';
+	req.body = {
+		message: 'success',
+	};
 });
 
 module.exports = router;
