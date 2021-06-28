41e8dcd93ecd5070aee769f77678a4d6133e45e4
doowankim <rlaendhks11@gmail.com>
2021-06-28 21:31:09 +0900

    소소한 수정

--- src/containers/Board/BoardDetail/index.tsx
+++ src/containers/Board/BoardDetail/index.tsx
@@ -7,16 +7,14 @@ const BoardDetail = () => {
 		comment: '',
 	});
 
-	const handleCommentChange = (e: any) => {
+	const handleChange = (e: any) => {
 		setComment({ ...comment, [e.target.name]: e.target.value });
 	};
 
 	const handleSubmit = (e: any) => {
 		e.preventDefault();
 
-		if (comment) {
-			window.location.reload(true);
-		}
+		console.log(comment);
 	};
 
 	return (
@@ -42,33 +40,31 @@ const BoardDetail = () => {
 							</React.Fragment>
 						))}
 					</Styled.TagBox>
-				</Styled.BottomBox>
-				<form onSubmit={handleSubmit}>
 					<Styled.CommentBox>
 						<Styled.CommentCount>0 Comment</Styled.CommentCount>
 						<Styled.CommentDetailBox>
 							<Styled.CommentUserName>
 								<input
 									name="name"
-									value={comment?.name}
+									value={comment.name}
 									placeholder="이름을 작성해주세요"
-									onChange={handleCommentChange}
+									onChange={handleChange}
 								/>
 							</Styled.CommentUserName>
 							<Styled.CommentText>
 								<textarea
 									name="comment"
-									value={comment?.comment}
+									value={comment.comment}
 									placeholder="소중한 댓글을 작성해주세요"
-									onChange={handleCommentChange}
+									onChange={handleChange}
 								/>
-								<Styled.CommentButton type="submit">
+								<Styled.CommentButton onClick={handleSubmit}>
 									Add Comment
 								</Styled.CommentButton>
 							</Styled.CommentText>
 						</Styled.CommentDetailBox>
 					</Styled.CommentBox>
-				</form>
+				</Styled.BottomBox>
 			</Styled.DescriptionWrapper>
 		</Styled.Container>
 	);
